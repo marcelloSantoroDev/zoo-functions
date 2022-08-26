00b90ecf6bd6ca181fac3c5be6d93a2c15e3bf09
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-08-26 17:04:43 -0300

    adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const getAnimal = species.find((element) => element.name === animal).residents;
+  return getAnimal.every((element) => element.age > age);
 }
 
 module.exports = getAnimalsOlderThan;

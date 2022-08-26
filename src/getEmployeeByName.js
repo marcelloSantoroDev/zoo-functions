a3c601616c0efe405920af747f6503adaaf53a11
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-08-26 17:16:54 -0300

    adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return employeeName === undefined ? {} : employees
+    .find((element) => element.firstName === employeeName || element.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;

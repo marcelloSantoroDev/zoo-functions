99055284b5adf4df7b13d47b8774bc79a7759374
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-08-26 18:14:07 -0300

    adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees
+    .some((element) => element.managers
+      .find((arg) => arg === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const checkManager = employees
+    .some((element) => element.managers
+      .find((arg) => arg === managerId));
+  if (checkManager) {
+    return employees
+      .filter((element) => element.managers.find((arg) => arg === managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };

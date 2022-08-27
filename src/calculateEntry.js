fc929d20ca1f7ebe9d8658c3b2c8bd191040cedb
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-08-27 12:22:34 -0300

    adiciona requisito 8 completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,5 +1,7 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
   const obj = { child: 0, adult: 0, senior: 0 };
   entrants.forEach((element) => {
@@ -14,6 +16,27 @@ function countEntrants(entrants) {
   return obj;
 }
 
+function calculateEntry(entrants) {
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entrantsArray = countEntrants(entrants);
+  let total = 0;
+  total += entrantsArray.child * prices.child;
+  total += entrantsArray.adult * prices.adult;
+  total += entrantsArray.senior * prices.senior;
+  return total;
+}
+
+console.log(calculateEntry([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]));
+
 // console.log(countEntrants([
 //   { name: 'Lara Carvalho', age: 5 },
 //   { name: 'Frederico Moreira', age: 5 },
@@ -22,12 +45,5 @@ function countEntrants(entrants) {
 //   { name: 'Núbia Souza', age: 18 },
 //   { name: 'Carlos Nogueira', age: 50 },
 // ]));
-
-function calculateEntry(entrants) {
-  // seu código aqui;
-  if (entrants === undefined) {
-    return 0;
-  }
-}
 
 module.exports = { calculateEntry, countEntrants };

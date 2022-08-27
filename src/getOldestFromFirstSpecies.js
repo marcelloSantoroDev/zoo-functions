cfcff84d62514753ecb012019ce57169201e324c
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-08-27 14:40:58 -0300

    adiciona requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstAnimal = employees.find((element) => element.id === id).responsibleFor[0];
+  const getAnimalObj = species.find((element) => element.id === firstAnimal).residents;
+  let getOldest = 0;
+  let count = 0;
+  getAnimalObj.forEach((element) => {
+    if (element.age > count) {
+      count = element.age;
+      getOldest = element;
+    }
+  });
+  const makeArray = [];
+  makeArray.push(getOldest.name);
+  makeArray.push(getOldest.sex);
+  makeArray.push(getOldest.age);
+  return makeArray;
 }
+
+console.log(getOldestFromFirstSpecies('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 module.exports = getOldestFromFirstSpecies;

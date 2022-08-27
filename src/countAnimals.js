b6c8d612c7112f6887c1f521bb69afbd94006d34
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-08-27 15:52:18 -0300

    adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
-// const { species } = data;
+const { species } = data;
+
+const getObj = () => {
+  const obj = {};
+  species.forEach((element) => {
+    obj[element.name] = element.residents.length;
+  });
+  return obj;
+};
 
 function countAnimals(animal) {
-
+  if (animal === undefined) {
+    return getObj();
+  }
+  const getSpecie = species.find((element) => element.name === animal.specie).residents;
+  const getMaleSex = getSpecie.filter((element) => element.sex === 'male');
+  const getFemaleSex = getSpecie.filter((element) => element.sex === 'female');
+  if (Object.keys(animal).length === 1) {
+    return getSpecie.length;
+  } if (animal.sex === 'male') {
+    return getMaleSex.length;
+  } if (animal.sex === 'female') {
+    return getFemaleSex.length;
+  }
 }
-
-console.log(countAnimals('lions'));
 
 module.exports = countAnimals;

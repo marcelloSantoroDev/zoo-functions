ba64e9b748362a3950a784d67d64eb522fd0c61d
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-08-28 19:14:30 -0300

    adiciona primeira questão do requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+// const { employees } = data;
+
+function getScheduleArray(animal) {
+  const animalSchedule = species
+    .find((element) => element.name === animal).availability;
+  return animalSchedule;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  let animalArray = 0;
+  species.forEach((element) => {
+    if (scheduleTarget === element.name) {
+      animalArray = getScheduleArray(scheduleTarget);
+    }
+  });
+  return animalArray;
 }
 
 module.exports = getSchedule;

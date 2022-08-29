4eaf7c9bc166b2a7cdcaf3acf7c44b7fc91e77b7
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-08-29 20:35:04 -0300

    adiciona requisito 10 sem refatorar complexidade

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,3 +1,57 @@
+// function ifSexMaleSorted() {
+//   const locationObj = { NE: [], NW: [], SE: [], SW: [] };
+//   species.forEach((element) => {
+//     if (element.location === 'NE') {
+//       locationObj.NE.push({ [element.name]: element.residents
+//         .filter((arg) => arg.sex === 'male').map((el) => el.name).sort() });
+//     } if (element.location === 'NW') {
+//       locationObj.NW.push({ [element.name]: element.residents
+//         .filter((arg) => arg.sex === 'male').map((el) => el.name).sort() });
+//     } if (element.location === 'SE') {
+//       locationObj.SE.push({ [element.name]: element.residents
+//         .filter((arg) => arg.sex === 'male').map((el) => el.name).sort() });
+//     } if (element.location === 'SW') {
+//       locationObj.SW.push({ [element.name]: element.residents
+//         .filter((arg) => arg.sex === 'male').map((el) => el.name).sort() });
+//     }
+//   });
+//   return locationObj;
+// }
+
+// function ifSexMale() {
+//   const locationObj = { NE: [], NW: [], SE: [], SW: [] };
+//   species.forEach((element) => {
+//     if (element.location === 'NE') {
+//       locationObj.NE.push({ [element.name]: element.residents
+//         .filter((arg) => arg.sex === 'male').map((el) => el.name) });
+//     } if (element.location === 'NW') {
+//       locationObj.NW.push({ [element.name]: element.residents
+//         .filter((arg) => arg.sex === 'male').map((el) => el.name) });
+//     } if (element.location === 'SE') {
+//       locationObj.SE.push({ [element.name]: element.residents
+//         .filter((arg) => arg.sex === 'male').map((el) => el.name) });
+//     } if (element.location === 'SW') {
+//       locationObj.SW.push({ [element.name]: element.residents
+//         .filter((arg) => arg.sex === 'male').map((el) => el.name) });
+//     }
+//   });
+//   return locationObj;
+// }
+
+// console.log(ifSexMale());
+
+// function sixthConditions(options) {
+//   const sexmaleSorted = Object.keys(options)
+//     .includes('includeNames') && Object.values(options)
+//     .includes('male') && Object.keys(options).includes('sorted');
+//   return sexmaleSorted;
+// }
+// function forthConditions(options) {
+//   const sexMale = Object.keys(options)
+//     .includes('includeNames') && Object.values(options).includes('male');
+//   return sexMale;
+// }
+
 const data = require('../data/zoo_data');
 
 const { species } = data;
@@ -55,28 +109,6 @@ function ifSortedIsTrue() {
 
 // console.log(ifSortedIsTrue());
 
-function ifSexMale() {
-  const locationObj = { NE: [], NW: [], SE: [], SW: [] };
-  species.forEach((element) => {
-    if (element.location === 'NE') {
-      locationObj.NE.push({ [element.name]: element.residents
-        .filter((arg) => arg.sex === 'male').map((el) => el.name) });
-    } if (element.location === 'NW') {
-      locationObj.NW.push({ [element.name]: element.residents
-        .filter((arg) => arg.sex === 'male').map((el) => el.name) });
-    } if (element.location === 'SE') {
-      locationObj.SE.push({ [element.name]: element.residents
-        .filter((arg) => arg.sex === 'male').map((el) => el.name) });
-    } if (element.location === 'SW') {
-      locationObj.SW.push({ [element.name]: element.residents
-        .filter((arg) => arg.sex === 'male').map((el) => el.name) });
-    }
-  });
-  return locationObj;
-}
-
-// console.log(ifSexMale());
-
 function ifSexFemale() {
   const locationObj = { NE: [], NW: [], SE: [], SW: [] };
   species.forEach((element) => {
@@ -92,26 +124,6 @@ function ifSexFemale() {
     } if (element.location === 'SW') {
       locationObj.SW.push({ [element.name]: element.residents
         .filter((arg) => arg.sex === 'female').map((el) => el.name) });
-    }
-  });
-  return locationObj;
-}
-
-function ifSexMaleSorted() {
-  const locationObj = { NE: [], NW: [], SE: [], SW: [] };
-  species.forEach((element) => {
-    if (element.location === 'NE') {
-      locationObj.NE.push({ [element.name]: element.residents
-        .filter((arg) => arg.sex === 'male').map((el) => el.name).sort() });
-    } if (element.location === 'NW') {
-      locationObj.NW.push({ [element.name]: element.residents
-        .filter((arg) => arg.sex === 'male').map((el) => el.name).sort() });
-    } if (element.location === 'SE') {
-      locationObj.SE.push({ [element.name]: element.residents
-        .filter((arg) => arg.sex === 'male').map((el) => el.name).sort() });
-    } if (element.location === 'SW') {
-      locationObj.SW.push({ [element.name]: element.residents
-        .filter((arg) => arg.sex === 'male').map((el) => el.name).sort() });
     }
   });
   return locationObj;
@@ -137,6 +149,11 @@ function ifSexFemaleSorted() {
   return locationObj;
 }
 
+function conditions(options) {
+  const a = Object.keys(options).includes('includeNames');
+  return a;
+}
+
 function firstConditions(options) {
   const ifError = !Object.keys(options)
     .includes('includeNames') && Object.keys(options).includes('sex');
@@ -146,14 +163,14 @@ function firstConditions(options) {
   return ifError || ifErrorSorted;
 }
 
-function firstConditionAndUndefined(options) {
-  const condition = options === undefined;
-  return condition || firstConditions(options);
+function getUndefined(options) {
+  return options === undefined || firstConditions(options);
 }
 
 function secondConditions(options) {
   const nameSorted = Object.keys(options)
-    .includes('includeNames') && Object.keys(options).includes('sorted');
+    .includes('includeNames') && Object.keys(options)
+    .includes('sorted') && Object.values(options).includes(true);
   return nameSorted;
 }
 
@@ -163,18 +180,19 @@ function thirdConditions(options) {
   return sexFemale;
 }
 
-function forthConditions(options) {
-  const sexMale = Object.keys(options)
-    .includes('includeNames') && Object.values(options).includes('male');
-  return sexMale;
+function fifthConditions(options) {
+  const sexFemaleSorted = Object.keys(options)
+    .includes('includeNames') && Object.values(options)
+    .includes('female') && Object.keys(options).includes('sorted');
+  return sexFemaleSorted;
 }
 
-function getAnimalMap(options) {
-  if (firstConditionAndUndefined(options)) return ifIsUndefined();
-  if (secondConditions(options)) return ifSortedIsTrue();
-  if (thirdConditions(options)) return ifSexFemale();
-  if (forthConditions(options)) return ifSexMale();
-}
-console.log(getAnimalMap());
+// function getAnimalMap(options) {
+//   if (getUndefined(options)) return ifIsUndefined();
+//   if (fifthConditions(options)) return ifSexFemaleSorted();
+//   if (thirdConditions(options)) return ifSexFemale();
+//   if (secondConditions(options)) return ifSortedIsTrue();
+//   if (conditions(options)) return ifIncludesNames();
+// }
 
 module.exports = getAnimalMap;
